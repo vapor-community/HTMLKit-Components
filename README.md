04e535c8ce2ab95f49482403d204e4223e6001ad
Matthias Mohr <mail@mattesmohr.de>
2021-09-11 19:36:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTMLKitComponents
+
+A description of this package.
---
+++ Tests/HTMLKitComponentsTests/HTMLKitComponentsTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import HTMLKitComponents
+
+    final class HTMLKitComponentsTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(HTMLKitComponents().text, "Hello, World!")
+        }
+    }
