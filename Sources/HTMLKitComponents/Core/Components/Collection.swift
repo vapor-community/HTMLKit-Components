d43a104f4fcd76cf3c1477fe77f07c38f42c7c90
Mattes Mohr <mail@mattesmohr.de>
2022-01-30 17:08:43 +0100

    Update the collection component

--- Sources/HTMLKitComponents/Core/Components/Collection.swift
+++ Sources/HTMLKitComponents/Core/Components/Collection.swift
@@ -1,81 +1,47 @@
 import HTMLKit
 
-public struct CollectionContainer: Component {
-    
-    private let content: AnyContent
-    
-    public init(@ContentBuilder<AnyContent> content: () -> AnyContent) {
-        self.content = content()
-    }
-    
-    public var body: AnyContent {
-        Division {
-            content
-        }
-        .class("collection")
-    }
-}
-
-public struct CollectionHeader: Component {
-    
-    private let content: AnyContent
-    
-    public init(@ContentBuilder<AnyContent> content: () -> AnyContent) {
-        self.content = content()
-    }
-    
-    public var body: AnyContent {
-        Division {
-            content
-        }
-        .class("collection-header")
-    }
-}
-
-public struct CollectionBody: Component {
+public struct Collection: Component {
     
     private let content: [ListElement]
+    private var classes: [String]
     
     public init(@ContentBuilder<ListElement> content: () -> [ListElement]) {
         self.content = content()
+        self.classes = ["collection"]
+    }
+    
+    internal init(content: [ListElement], classes: [String]) {
+        self.content = content
+        self.classes = classes
     }
     
     public var body: AnyContent {
         UnorderedList {
             content
         }
-        .class("collection-body")
+        .class(classes.joined(separator: " "))
     }
 }
 
 public struct CollectionItem: Component {
     
     private let content: AnyContent
+    private var classes: [String]
     
     public init(@ContentBuilder<AnyContent> content: () -> AnyContent) {
         self.content = content()
+        self.classes = ["collection-item"]
+    }
+    
+    internal init(content: AnyContent, classes: [String]) {
+        self.content = content
+        self.classes = classes
     }
     
     public var body: AnyContent {
         ListItem {
             content
         }
-        .class("collection-item")
-    }
-}
-
-public struct CollectionFooter: Component {
-    
-    private let content: AnyContent
-    
-    public init(@ContentBuilder<AnyContent> content: () -> AnyContent) {
-        self.content = content()
-    }
-    
-    public var body: AnyContent {
-        Division {
-            content
-        }
-        .class("collection-footer")
+        .class(classes.joined(separator: " "))
     }
 }
